14bd3af06172d8d64886aa7e77bd298851f5a6a4
Hector Badillo <chelitobulgati@gmail.com>
2025-03-07 21:31:04 -0600

    Servicio get y post

---
+++ Primer proyecto con Spring Boot/demo/src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,60 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false)
+    private Long id;
+    
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+    // Getter y Setter para id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter y Setter para nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter y Setter para email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter y Setter para prioridad
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+}
+
