29934c6f0e4c91323fc404b3932eb26842a97915
Hector Badillo <chelitobulgati@gmail.com>
2025-03-31 17:08:22 -0600

    Tarea mandar correos

---
+++ Correos/spring-boot/src/main/java/com/example/spring_boot/services/EmailService.java
@@ -0,0 +1,22 @@
+package com.example.spring_boot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    JavaMailSender mailSender;
+
+    public void enviarEmail(String destino, String asunto, String texto){
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+        mensaje.setTo(destino);
+        mensaje.setSubject(asunto);
+        mensaje.setText(texto);
+        mensaje.setFrom("ssjgssj1717@gmail.com");
+
+        mailSender.send(mensaje);
+    }
+}
